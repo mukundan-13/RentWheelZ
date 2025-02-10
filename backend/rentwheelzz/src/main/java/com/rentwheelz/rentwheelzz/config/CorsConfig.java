80b9ad22046db2190652fde0a9727f79c23c85a7
MUKUNDAN D <mukundanproff@gmail.com>
2025-02-11 00:31:30 +0530

    Update CorsConfig.java

--- backend/rentwheelzz/src/main/java/com/rentwheelz/rentwheelzz/config/CorsConfig.java
+++ backend/rentwheelzz/src/main/java/com/rentwheelz/rentwheelzz/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
       
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") 
+                .allowedOrigins("https://rentwheelz.netlify.app") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
 
